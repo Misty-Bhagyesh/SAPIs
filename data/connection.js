b888776334e84f3eb855e82ea9a39d9e7a6e48a5
Bhagyesh <contact@scrappi.in>
2023-10-07 13:48:28 +0530

    Update connection.js

--- data/connection.js
+++ data/connection.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const db_url = process.env.DB_URL;
 
-const connect = function () {
-    mongoose.connect(db_url).then((res)=>{
-        console.log(`Database hase connected!`)
-    }).catch(err=>{
-        console.error(`error on connecting database!`);
-    });
-  
+const connect = async () => {
+    try {
+        await mongoose.connect(db_url).then(() => {
+            console.log(`Database hase connected!`)
+        })
+    } catch (error) {
+        handleError(error);
+    }
 }
+
 
 module.exports = connect;
